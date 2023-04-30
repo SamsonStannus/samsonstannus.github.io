d3b9f974d489eef156e79d48b1e9d9d140a10aeb
SamsonStannus <samsonstannus@gmail.com>
2023-04-30 14:56:14 -0400

    remove export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
     images: {
-      loader: 'imgix',
-      path: 'https://samsonstannus.imgix.net',
-    },
+        loader: 'imgix',
+        path: 'https://samsonstannus.imgix.net',
+      },
   }
+  
+module.exports = nextConfig
